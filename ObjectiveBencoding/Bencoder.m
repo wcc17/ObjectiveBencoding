3340167b4c0482cc83e7b33853efd364f848871c
Christian Curry <christian.curry17@gmail.com>
2016-09-18 14:46:29 -0400

    completed methods for encoding string an integer values

--- ObjectiveBencoding/Bencoder.m
+++ ObjectiveBencoding/Bencoder.m
@@ -15,10 +15,10 @@
 + (NSString *)encode:(NSObject *)objectToEncode {
     if([objectToEncode isKindOfClass:[NSString class]]) {
         NSLog(@"Object is a string with value: %@", objectToEncode);
-        [self encodeString:((NSString *)objectToEncode)];
+         return [self encodeString:((NSString *)objectToEncode)];
     } else if([objectToEncode isKindOfClass:[NSNumber class]]) {
         NSLog(@"Object is an integer with value: %@", objectToEncode);
-        [self encodeInteger:((NSNumber *)objectToEncode)];
+        return [self encodeInteger:((NSNumber *)objectToEncode)];
     }
     
     return nil;
@@ -27,13 +27,34 @@
 + (NSString *) encodeString:(NSString *) stringToEncode {
     NSLog(@"String to encode: %@", stringToEncode);
     
-    return nil;
+//    Byte strings are encoded as follows: <string length encoded in base ten ASCII>:<string data>
+//    Note that there is no constant beginning delimiter, and no ending delimiter.
+//    
+//    Example: 4:spam represents the string "spam"
+//    Example: 0: represents the empty string ""   TODO: this will be a special case in decoding these
+    
+    NSInteger stringLength = stringToEncode.length;
+    NSString *encodedString = [NSString stringWithFormat:@"%tu:%@", stringLength, stringToEncode];
+    
+    return encodedString;
 }
 
 + (NSString *) encodeInteger:(NSNumber *) integerToEncode {
     NSLog(@"Integer to encode: %@", integerToEncode);
     
-    return nil;
+//    Integers are encoded as follows: i<integer encoded in base ten ASCII>e
+//    The initial i and trailing e are beginning and ending delimiters.
+//    
+//    Example: i3e represents the integer "3"
+//    Example: i-3e represents the integer "-3"
+//    i-0e is invalid. All encodings with a leading zero, such as i03e, are invalid, other than i0e, which of course corresponds to the integer "0".
+//    
+//    NOTE: The maximum number of bit of this integer is unspecified, but to handle it as a signed 64bit integer is mandatory to handle "large files" aka .torrent for more that 4Gbyte.
+    
+    NSString *encodedInteger;
+    encodedInteger = [NSString stringWithFormat:@"i%@e", [integerToEncode stringValue]];
+    
+    return encodedInteger;
 }
 
 @end
--- ObjectiveBencoding/main.m
+++ ObjectiveBencoding/main.m
@@ -11,11 +11,36 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSString *testString1 = [Bencoder encode:@"EncodeThisString"];
+        NSString *testString2 = [Bencoder encode:@"Encode this string too"];
+        NSString *testString3 = [Bencoder encode:@" "];
+        NSString *testString4 = [Bencoder encode:@""];
         
-        [Bencoder encode:@"EncodeThisString"];
-        [Bencoder encode:[NSNumber numberWithInt:1]];
+        //test integers
+        NSString *testInteger1 = [Bencoder encode:[NSNumber numberWithInt:1]];
+        NSString *testInteger2 = [Bencoder encode:[NSNumber numberWithInt:0]];
+        NSString *testInteger3 = [Bencoder encode:[NSNumber numberWithInt:-3]];
+        NSString *testInteger4 = [Bencoder encode:[NSNumber numberWithInt:27]];
+        NSString *testInteger5 = [Bencoder encode:[NSNumber numberWithLong: 382340958309458930]]; //def larger than 2147483647, to prove 64 bit int is possible
+        NSString *testInteger6 = [Bencoder encode:[NSNumber numberWithInt:-0]]; //weird that this -0 is allowed so testing
+        
+        NSLog(@" ");
+        
+        NSLog(@"Encoded string, no spaces: %@", testString1);
+        NSLog(@"Encoded string with spaces: %@", testString2);
+        NSLog(@"Encoded string only a space: %@", testString3);
+        NSLog(@"Encoded empty string: %@", testString4);
+        
+        NSLog(@" ");
+        
+        //print encoded integers
+        NSLog(@"Encoded normal integer: %@", testInteger1);
+        NSLog(@"Encoded zero: %@", testInteger2);
+        NSLog(@"Encoded negative number: %@", testInteger3);
+        NSLog(@"Encoded integer with more than 1 digit: %@", testInteger4);
+        NSLog(@"Encoded integer greater than 32 bits: %@", testInteger5);
+        NSLog(@"Encoded negative zero: %@", testInteger6);
+        
     }
     return 0;
 }
